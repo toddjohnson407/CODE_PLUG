57005e9e2822877b6f2c3832a739be5664246a10
Arthur Pfister <arthr.pfister@gmail.com>
2018-06-07 18:38:58 +0100

    ajax filtering working but need to refresh maps when it does

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,8 +6,8 @@ class CoursesController < ApplicationController
   end
 
   def index
-    @courses = policy_scope(Course).where.not(latitude: nil, longitude: nil)
-    @courses = Course.search_by_city_and_address(params[:search])
+    # @courses = policy_scope(Course).where.not(latitude: nil, longitude: nil)
+    # @courses = Course.search_by_city_and_address(params[:search])
 
     @markers = @courses.map do |course|
       {
@@ -15,6 +15,12 @@ class CoursesController < ApplicationController
         lng: course.longitude
       }
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def show
