3b72a59cab29493a238eea3965efe38ea030fbd1
Arthur Pfister <arthr.pfister@gmail.com>
2018-06-12 11:08:05 +0100

    added dates to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_221949) do
+ActiveRecord::Schema.define(version: 2018_06_12_100737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
