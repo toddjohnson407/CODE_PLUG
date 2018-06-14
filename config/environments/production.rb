4df34eff0dcda301b667761e2413fc941d4ac889
Todd Johnson <toddjohnson407@gmail.com>
2018-06-14 11:00:35 -0600

    added allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://codeplug0.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://codeplug0.herokuapp.com/', 'http://codeplug.co' ]
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.allowed_request_origins = [ 'https://codeplug0.herokuapp.com/', 'http://codeplug.co', 'http://codeplug.co']
+  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
