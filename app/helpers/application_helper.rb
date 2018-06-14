65dc5b462ed47467517685b848544e660967c497
Todd Johnson <toddjohnson407@gmail.com>
2018-06-14 06:15:55 -0600

    wow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
   def self.get_image_name_from_subject(subject)
     case subject
     when "c++"
-      "c"
+      "c.png"
     when "html/css"
-      "html"
-    else  
-      subject
+      "html.png"
+    else
+      subject + ".png"
     end
   end
 end
